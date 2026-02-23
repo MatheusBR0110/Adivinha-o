bae88a3e4d823e13ed707e3ab6769c7bac3323ff
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-02-23 11:50:02 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+import random as rd
+
 print("------------")
 print("Bem-Vindo ao jogo de adivinhação!")
 print("------------")
 
-n_secreto = 50947
-n_tentativas = 10
+n_secreto = rd.randranger
+n_tentativas = 3
 rodada = 1
 
 for rodada in range(1,n_tentativas + 1):
     print(f"Tentativa {rodada} de {n_tentativas}")
-    entrada = int(input("Digite um número: "))
+    entrada = int(input("Digite um número de 1 a 100: "))
     acerto = entrada == n_secreto
     entrada_maior = entrada == n_secreto
     entrada_menor = entrada < n_secreto
+
+    if(entrada > 100 or entrada < 1):
+        print("O valor digitado não é permitido")
+        continue
 
     print(f"Você digitou o número: {entrada}")
 
---
+++ teste.py
@@ -0,0 +1,3 @@
+import random as rd
+testeRandom = round(rd.random() * 60000)
+print(int(testeRandom))
